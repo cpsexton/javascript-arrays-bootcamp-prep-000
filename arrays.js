4cf84db73e624f0a8d5b5040e8c6b764eed94800
Christopher Sexton <cpsexton@gmail.com>
2019-07-23 18:24:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,2 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-addElementToBeginningOfArray = chocolateBars.push("mars bars")
+let addElementToBeginningOfArray = chocolateBars.push("mars bars");
