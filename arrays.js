ba46c6b198daee986f35e3d55eec0c981e0624c5
Christopher Sexton <cpsexton@gmail.com>
2019-07-23 19:07:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,6 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
   return chocolateBars
 }
 function removeElementFromEndOfArray(chocolateBars) {
-  chocolateBars = chocolateBars.pop()
+  chocolateBars = chocolateBars.slice(4)
   return chocolateBars
 }
